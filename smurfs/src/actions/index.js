bdc9ff9cedb495819add0e459352e93530fdd07a
Christian-Fjord <fjord.christian@gmail.com>
2019-04-20 11:41:30 -0500

    Added actions to action/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,32 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCHING_SMURFS = "FETCHING_SMURFS";
+export const SMURF_FETCH_SUCCESS = "SMURF_FETCH_SUCCESS";
+export const SMURF_FETCH_FAILURE = "SMURF_FETCH_FAILURE";
+export const ADD_SMURF = "ADD_SMURF";
+export const ADDED_SMURF = "ADDED_SMURF";
+export const ADD_ERROR = "ADD_ERROR";
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({type: FETCHING_SMURFS});
+  axios
+    .get(`http://localhost:3333/smurfs`)
+    .then(response => {
+      dispatch({type: SMURF_FETCH_SUCCESS, payload: response.data})
+    })
+    .catch(error => {
+      dispatch({type: SMURF_FETCH_FAILURE, payload: error});
+    })
+}
+
+export const addNewSmurf = (newSmurf) => dispatch => {
+  dispatch({ type: ADDING_SMURF });
+  axios 
+  .post('http://localhost:3333/smurfs', newSmurf)
+  .then( response => {
+    dispatch({ type: SMURF_ADDED, payload: response.data});
+  })
+  .catch(error => {
+    dispatch({ type: ADD_SMURF_ERROR, payload: error});
+  });
+}; 
